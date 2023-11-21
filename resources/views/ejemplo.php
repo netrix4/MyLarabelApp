6133ecedfe224159478e3dc30c6a57ba8f8ab954
Mario Arias <netrix4@gmail.com>
2023-11-21 10:20:37 -0800

    UpTodate with teachers example posts

---
+++ resources/views/ejemplo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- <link rel="stylesheet" href="../resources/styles.css"> -->
+        <title>Proyecto Compilador L&A1</title>
+    </head>
+    <body>
+        <div class='card-element-wrapper'>
+            <div class='card-element-header'>
+                <h2>Analizador Léxico</h2>
+            </div>
+            <div class='card-element-content' id="cardContent">
+                <label class='text-center' htmlFor="inputAutom">Click en el botón para explorar por un archivo de texto</label>
+                <input type="file" id="selectedFile" accept=".txt" />
+                <textarea id="inputText"></textarea>
+                <button id="download">Descargar</button>
+            </div>
+        </div>
+        <!-- <script src="./Practica7.js" type="module" ></script> -->
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,60 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+ 
+});*/
+   /** 
+Route::get('/prueba', function () {
+    echo"Esto es una simple prueba!!";
 });
+
+Route::get('colaboradores/{nombre}', function($nombre){
+	return "Mostrando el colaborador $nombre";
+});
+
+Route::get('vista', function () {
+    return view('ejemplo');
+});
+
+Route::get('vista/{enero},{anio},{evento}', function ($mes,$anio,$eventos) {
+    return view('ejemplo', [
+    	'mes' => $mes,
+    	'ano' => $anio,
+    	'eventos' => $eventos]);
+       
+}); 
+
+Route::get('articulos', function(){
+	dd(\App\Articulos::all());
+});**/
+
+// returns the home page with all posts
+Route::get('/', PostController::class .'@index')->name('posts.index');
+// returns the form for adding a post
+Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+// adds a post to the database
+Route::post('/posts', PostController::class .'@store')->name('posts.store');
+// returns a page that shows a full post
+Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
+// returns the form for editing a post
+Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
+// updates a post
+Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+// deletes a post
+Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
+
+
